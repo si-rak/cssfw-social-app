8008aecf0d8604f09d44f46037175231aa4b66bf
si-rak <zebeaman3@gmail.com>
2025-10-06 23:25:13 +0200

    Revise project structure for CSS Frameworks

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./*.html", "./feed/**/*.html", "./user-profile/**/*.html"]
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
